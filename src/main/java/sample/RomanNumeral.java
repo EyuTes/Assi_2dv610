5169540b9b90ce75491affffda692ed06fcb1940
eyuel <eyueltb@gmail.com>
2018-11-28 23:23:29 +0100

    TDD without implementing the logic. Test fails(Red).

--- src/main/java/sample/RomanNumeral.java
+++ src/main/java/sample/RomanNumeral.java
@@ -1,4 +1,23 @@
 package sample;
+import java.util.Map;
+import java.util.HashMap;
+/**
+ * This class converts roman number to number numbers
+ */
 
 public class RomanNumeral {
+    private static Map<Character,Integer>map;
+    static {
+        map=new HashMap<Character,Integer>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+    }
+    public int convert(String s) {
+        return 0;
+    }
 }
--- src/test/java/sample/RomanNumeralTest.java
+++ src/test/java/sample/RomanNumeralTest.java
@@ -1,7 +1,23 @@
 package sample;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+/**
+ * This is a test class that tests the validity of conversion form roman to numeral
+ */
 class RomanNumeralTest {
+    private RomanNumeral roman;
 
+    @BeforeEach
+    public void initialize() {
+        this.roman = new RomanNumeral();
+    }
+
+    @Test
+    void singleNumber() {
+        int result = roman.convert("I");
+        assertEquals(1, result);
+    }
 }
