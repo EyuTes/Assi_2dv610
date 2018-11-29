8acb57c3844be23fb97678289f683114cf1024f0
eyuel <eyueltb@gmail.com>
2018-11-29 01:39:19 +0100

    Add DisplayName annotation! All test cases are Green

--- src/test/java/sample/NumuberFinderTest.java
+++ src/test/java/sample/NumuberFinderTest.java
@@ -3,6 +3,7 @@ package sample;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.Map;
 
@@ -12,46 +13,55 @@ class NumuberFinderTest {
     void setUp() {
         numberFinder=new NumuberFinder();
     }
+    @DisplayName("Get the smallest number for random array")
     @Test
     void getSmallestWhenArrayIsRandom() {
         numberFinder.find(new int[]{4,25,7,9});
         Assertions.assertEquals(4,numberFinder.getSmallest());
     }
+    @DisplayName("Get the largest number for random array")
     @Test
     void getLargestWhenArrayIsRandom() {
         numberFinder.find(new int[]{100,-12,0,45,1,-25});
         Assertions.assertEquals(100, numberFinder.getLargest());
     }
+    @DisplayName("Get the smallest number when array is in increasing order")
     @Test
     void getSmallestWhenArrayIncreasingOrder(){
         numberFinder.find(new int[]{-2,0,12,30,45});
         Assertions.assertEquals(-2, numberFinder.getSmallest());
     }
+    @DisplayName("Get the largest number when array is in increasing order")
     @Test
     void getLargestWhenArrayIncreasingOrder(){
         numberFinder.find(new int[]{-2,0,12,30,45,120});
         Assertions.assertEquals(120, numberFinder.getLargest());
     }
+    @DisplayName("Get the largest number when array is in decreasing order")
     @Test
     void getLargestWhenArrayDecreasingOrder(){
         numberFinder.find(new int[]{300,100,12,5,0,-12,-90});
         Assertions.assertEquals(-90, numberFinder.getSmallest());
     }
+    @DisplayName("Get the smallest number when array is in decreasing order")
     @Test
     void getSmallestWhenArrayDecreasingOrder(){
         numberFinder.find(new int[]{350,300,100,12,5,0,-12,-90});
         Assertions.assertEquals(350, numberFinder.getLargest());
     }
+    @DisplayName("Get the smallest number when array contains the same numbers")
     @Test
     void getSmallestWhenArraySameNumber(){
         numberFinder.find(new int[]{7,7,7,7,7});
         Assertions.assertEquals(7, numberFinder.getSmallest());
     }
+    @DisplayName("Get the largest number when array contains the same numbers")
     @Test
     void getLargestWhenArraySameNumber(){
         numberFinder.find(new int[]{0,0,0,0,0,0});
         Assertions.assertEquals(0, numberFinder.getLargest());
     }
+    @DisplayName("Get the smallest& largest number when array random")
     @Test
     void getSmallesAndLargest(){
         Map<Integer,Integer> numFind=numberFinder.findNum(new int[]{12,15,7,3,30});
--- src/test/java/sample/RomanNumeralTest.java
+++ src/test/java/sample/RomanNumeralTest.java
@@ -2,6 +2,7 @@ package sample;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -14,43 +15,45 @@ class RomanNumeralTest {
     public void initialize() {
         this.roman = new RomanNumeral();
     }
-
+    @DisplayName("For single roman number")
     @Test
     void singleNumber() {
         int result = roman.convert("I");
         assertEquals(1, result);
     }
+    @DisplayName("For roman number with multiple digit")
     @Test
     void NumberWithMultipleDigits() {
         int result = roman.convert("VIII");
         assertEquals(8, result);
     }
+    @DisplayName("For roman number with different digit")
     @Test
     void numberWithDifferentDigits() {
         int result = roman.convert("XVI");
         assertEquals(16, result);
     }
-
+    @DisplayName("For roman number with subtractive notation")
     @Test
     void numberWithSubtractiveNotations() {
         int result = roman.convert("IX");
         assertEquals(9, result);
         //Assertions.assertEquals(9, result);
     }
-
+    @DisplayName("For roman number with digit & subtractive notation")
     @Test
     void numberWithDigitAndSubtractiveNotaions() {
         RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("XIX");
         assertEquals(19, result);
     }
-
+    @DisplayName("For roman number with subtractive notation")
     @Test
     public void numberWithSubtractiveNotation() {
         int result = roman.convert("IV");
         assertEquals(4, result);
     }
-
+    @DisplayName("For roman number with out subtractive notation")
     @Test
     public void numberWithAndWithoutSubtractiveNotation() {
         int result = roman.convert("XLIV");
