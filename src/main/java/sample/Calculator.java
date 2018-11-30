f87acacfde2ce5df79073b0410dab304167a993d
eyuel <eyueltb@gmail.com>
2018-11-30 10:21:29 +0100

    calculator logic with out implementing any code but testing different case using assertAll!(Red)

--- src/main/java/sample/Calculator.java
+++ src/main/java/sample/Calculator.java
@@ -4,7 +4,9 @@ package sample;
  * This class acts as a calculator to sum, subtract, multiply numbers or array of numbers
  */
 public class Calculator {
-    public int sum(int firstNumber, int secondNumber){
-      return 0;
+
+
+    public int sum(int [] numbers){
+        return 5;
     }
 }
--- src/test/java/sample/CalculatorTest.java
+++ src/test/java/sample/CalculatorTest.java
@@ -1,8 +1,11 @@
 package sample;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
     private Calculator calculator;
@@ -10,10 +13,19 @@ class CalculatorTest {
     void setUp() {
         calculator=new Calculator();
     }
+
+    @DisplayName("Get sum of Array of integer using assert all")
     @Test
-    void getSumOfNumbers() {
-        int actual=calculator.sum(1,4);
-        int expected=5;
-        Assertions.assertEquals(5,actual);
+
+     void getSumOfNumbers() {
+        assertAll("sums of different integers",
+                ()->assertEquals(10,calculator.sum(new int[]{1,2,3,4})),
+                ()->assertEquals(40,calculator.sum(new int[]{9,12,15,4})),
+                ()->assertEquals(400,calculator.sum(new int[]{200,100,50,40,10,0})),
+                ()->assertEquals(8,calculator.sum(new int[]{-10,20,-50,40,0,8}))
+        );
+
     }
+
+
 }
