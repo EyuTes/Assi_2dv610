03588fcabdc36a46a940161045dbb3acd0085ae1
eyuel <eyueltb@gmail.com>
2018-11-30 11:06:10 +0100

    refactor for calculator test!(Green)

--- src/test/java/sample/CalculatorTest.java
+++ src/test/java/sample/CalculatorTest.java
@@ -3,7 +3,7 @@ package sample;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
@@ -17,7 +17,12 @@ class CalculatorTest {
     @Test
 
      void testValidSumOfNumbers() {
-        assertEquals(10,calculator.sum(new int[]{1,2,3,4}));
+        assertAll("sums of different integers",
+                ()->assertEquals(10,calculator.sum(new int[]{1,2,3,4})),
+                ()->assertEquals(40,calculator.sum(new int[]{9,12,15,4})),
+                ()->assertEquals(400,calculator.sum(new int[]{200,100,50,40,10,0})),
+                ()->assertEquals(8,calculator.sum(new int[]{-10,20,-50,40,0,8}))
+        );
     }
 
 
