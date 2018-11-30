9274c27ad19f45c6a8dd75be65acaf0ed22c7930
eyuel <eyueltb@gmail.com>
2018-11-30 11:38:07 +0100

    Refactor the filter method then multiple test for calculator test class passes! (All green)

--- src/main/java/sample/Calculator.java
+++ src/main/java/sample/Calculator.java
@@ -1,8 +1,8 @@
 package sample;
 
 import java.util.List;
-import java.util.ArrayList;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * This class acts as a calculator to sum, subtract, multiply numbers or array of numbers
@@ -17,12 +17,7 @@ public class Calculator {
        return sum;
     }
     public List<Integer> filters(List<Integer> integerList, Predicate<Integer> predicate){
-        List<Integer> filteredNumbers=new ArrayList<>();
-        for(Integer number:integerList){
-            if(predicate.test(number)){
-                filteredNumbers.add(number);
-            }
-        }
-        return filteredNumbers;
-    }
+          return integerList.stream().filter(predicate).collect(Collectors.toList());
+   }
+
 }
